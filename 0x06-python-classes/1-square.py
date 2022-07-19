423667b84167850f1e4df9573945c7c4c0be16a5
Necherem Gift <ilobigift@gmail.com>
2022-07-19 21:55:05 +0100

    Squareee

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" 
+This is Square module doc
+"""
+
+class Square:
+    """Represents a square.
+    Private instance attribute: size.
+    Instantiation with size (no type/value verification).
+    """
+
+    def __init__(self, size):
+        self.__size = size
